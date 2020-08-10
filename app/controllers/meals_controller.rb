b83d8f9eb6b203acb3aaf6d48f35c9e95f6ef9b0
webdevjscoder <cobianjuan19@gmail.com>
2020-08-10 16:50:02 -0700

    add create option for user

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -15,4 +15,23 @@ class MealsController < ApplicationController
         }
         render json: MealSerializer.new(meal, options)
     end
+
+    def create
+        meal = Meal.new(meal_params(:name, :description, :price))
+        meal.category_id = params[:categoryId]
+        if meal.save
+            options = {
+                include: [:category]
+            }
+            render json: MealSerializer.new(meal, options)
+        else
+            render json: {message: 'Meal was not created try again!'}
+        end
+    end
+
+    private
+
+    def meal_params(*args)
+        params.require(:meal).permit(*args)
+    end
 end
